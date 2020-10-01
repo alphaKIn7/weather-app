326f5d9f510b067c5873500df7e356d25f3d0a42
Shubham Gupta <71041665+alphaKIn7@users.noreply.github.com>
2020-10-01 23:56:06 +0530

    Setup Bottom Navigation

---
+++ app/src/main/java/com/example/weatherapp2/ui/settings/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.weatherapp2.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+}
---
+++ app/src/main/java/com/example/weatherapp2/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp2.ui.weather.current
+
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
